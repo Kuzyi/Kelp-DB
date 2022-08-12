ca403ffc816276c4807580adf9e21dce9161e46a
Cory <cjbenicak@gmail.com>
2022-08-12 10:22:08 -0400

    Updated the comments model with location and user ids

--- models/comment.js
+++ models/comment.js
@@ -15,7 +15,23 @@ module.exports = (sequelize, DataTypes) => {
   }
   Comment.init(
     {
-      content: DataTypes.STRING
+      content: DataTypes.STRING,
+      userId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      locationId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'locations',
+          key: 'id'
+        }
+      }
     },
     {
       sequelize,
