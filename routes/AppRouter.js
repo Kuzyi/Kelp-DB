c60907371a2b4211388482f9ce17d920ac143a4c
Cory <cjbenicak@gmail.com>
2022-08-12 14:15:21 -0400

    finished comment controllers and route for CRUD ops

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express').Router()
 const UserRouter = require('./user')
 const locationRouter = require('./location')
+const commentRouter = require('./comment')
 
 Router.use('/users', UserRouter)
 Router.use('/locations', locationRouter)
+Router.use('/comments', commentRouter)
 
 module.exports = Router
