919d200deba38232eb70e182fe62bd2d53a214a7
Timothy Mei <timothymei3270@gmail.com>
2022-08-12 12:30:30 -0700

    added route restriction for create comment

--- routes/comment.js
+++ routes/comment.js
@@ -1,8 +1,14 @@
 const Router = require('express').Router()
 const controller = require('../controllers/comment')
+const middleware = require('../middleware')
 
 Router.get('/all', controller.findAllComments)
-Router.post('/', controller.createComment)
+Router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.createComment
+)
 Router.put('/:commentId', controller.updateComment)
 Router.delete('/:commentId', controller.deleteComment)
 
